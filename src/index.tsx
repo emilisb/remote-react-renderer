ce66c023132b9f93ef3178def46ebab636e3c580
Tom Peres <tomp@wix.com>
2021-07-14 20:05:19 +0300

    mo cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,15 @@ import { createPlainWorkerFactory, createWorkerFactory } from '@remote-ui/web-wo
 
 const noop = (..._args: any[]) => () => {}
 
-const foo = noop(createPlainWorkerFactory(() =>
+noop(createPlainWorkerFactory(() =>
   import(/* webpackChunkName: 'foo' */ './components/foo'),
 ));
 
-foo()
-
-const bar = noop(createPlainWorkerFactory(() =>
+noop(createPlainWorkerFactory(() =>
   import(/* webpackChunkName: 'bar' */ './components/bar'),
 ));
 
-bar()
-
-const sandbox = noop(createWorkerFactory(() =>
+noop(createWorkerFactory(() =>
   import(/* webpackChunkName: 'sandbox' */ './sandbox'),
 ));
-
-sandbox()
 
