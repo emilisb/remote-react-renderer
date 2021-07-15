18e85382e7565a5710cd8be14b9d7c5fc5ab990b
Tom Peres <tomp@wix.com>
2021-07-15 20:03:13 +0300

    allow re-render of remote component

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -2,6 +2,7 @@ import {
   retain,
   createRemoteRoot,
   RemoteChannel,
+  release,
 } from '@remote-ui/core';
 
 import {endpoint} from '@remote-ui/web-workers/worker';
@@ -33,18 +34,36 @@ Reflect.defineProperty(self, 'doSomethingOnMainThread', {
   writable: false,
 });
 
+const _state = {
+  firstRun: true,
+  prevProps: {},
+  url: ''
+}
+
 // This method will be exposed to the worker thread by 
 export function run(script: string, channel: RemoteChannel, hostProps: HostProps) {
-  // `channel` is a function, which is proxied over from the main thread. If you ever
-  // "hold on" to a function you receive this way in order to call it later, you
-  // **must** call `retain()` in order to prevent it from being automatically garbage
-  // collected.
-  retain(channel);
+  if (_state.firstRun) {
 
-  // `user` contains functions, so it also needs to be retained.
-  retain(hostProps);
+      // `channel` is a function, which is proxied over from the main thread. If you ever
+      // "hold on" to a function you receive this way in order to call it later, you
+      // **must** call `retain()` in order to prevent it from being automatically garbage
+      // collected.
+      retain(channel);
 
-  importScripts(script);
+      // `hostProps` contains functions, so it also needs to be retained.
+      _state.prevProps = retain(hostProps) && hostProps
+
+  } else {
+    retain(hostProps)
+
+    if (_state.prevProps) {
+      release(_state.prevProps)
+    }
+  }
+
+  if (_state.url !== script) {
+    importScripts(script);
+  }
 
   if (renderCallback == null) {
     throw new Error(`The ${script} script did not register a callback to render UI. Make sure that code runs self.onRender().`)
@@ -53,4 +72,6 @@ export function run(script: string, channel: RemoteChannel, hostProps: HostProps
   const root = createRemoteRoot(channel, {components: []});
 
   renderCallback(root, hostProps);
+
+  _state.firstRun = false
 }
