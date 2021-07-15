290637890e989cbd8102d1e16d5c45b698e775a5
Tom Peres <tomp@wix.com>
2021-07-15 15:39:39 +0300

    nicer foo and bar

--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Bar = ({hostProps}: any) => <div>I am Bar</div>
+const Bar = ({hostProps}: any) => <div style={{ backgroundColor: '#3aac91', fontSize: 'x-large' }}>
+I am Bar and I run in a web worker!
+<pre>`self.location.href` is {self.location.href}</pre>
+<pre>`self.performance.timeOrigin` is {self.performance.timeOrigin}</pre>
+The viewer sent me prop:
+<pre>`self.location.href` is {hostProps.myProp}</pre>
+</div>
 
 export default Bar
--- src/components/foo.tsx
+++ src/components/foo.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Foo = ({hostProps}: any) => <div>I am Foo</div>
+const Foo = ({hostProps}: any) => <div style={{ backgroundColor: '#e383d8', fontSize: 'x-large' }}>
+I am Foo and I run in a web worker!
+<pre>`self.location.href` is {self.location.href}</pre>
+<pre>`self.performance.timeOrigin` is {self.performance.timeOrigin}</pre>
+The viewer sent me prop:
+<pre>`self.location.href` is {hostProps.myProp}</pre>
+</div>
 
 export default Foo
