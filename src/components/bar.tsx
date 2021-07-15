786da8fbe8de862b748c1451c4fed6a8a54f1a00
Tom Peres <tomp@wix.com>
2021-07-15 22:33:42 +0300

    fixed msg

--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -5,7 +5,7 @@ I am Bar and I run in a web worker!
 <pre>`self.location.href` is {self.location.href}</pre>
 <pre>`self.performance.timeOrigin` is {self.performance.timeOrigin}</pre>
 The viewer sent me prop:
-<pre>`self.location.href` is {hostProps.myProp}</pre>
+<pre>`myProp` is {hostProps.myProp}</pre>
 </div>
 
 export default Bar
