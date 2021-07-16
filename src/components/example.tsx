3f10c1ac8db44c053ebb26db7b75586bad4f4719
Tom Peres <tomp@wix.com>
2021-07-16 11:15:46 +0300

    imporved example comp

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
 
+const buttonClass = `material-ui-button`
+const buttonCss = `.${buttonClass}{box-sizing:border-box;display:inline-block;position:relative;cursor:pointer;height:35px;line-height:35px;padding:0 1.5rem;font-size:15px;font-weight:600;font-family:Roboto,sans-serif;letter-spacing:.8px;text-align:center;text-decoration:none;text-transform:uppercase;vertical-align:middle;white-space:nowrap;outline:0;border:none;user-select:none;border-radius:2px;transition:all .3s ease-out;box-shadow:0 2px 5px 0 rgba(0,0,0,.225);color:#fff;background-color:#03a9f4}`
+const globalCss = ``
+const css = [globalCss, buttonCss].join('\n')
+
 function ExampleApp({hostProps}: any) {
+  
   const [clicks, setClicks] = useState({
     button: 0,
     div1: 0
   });
-  const [mainThreadRes, setMainThreadRes] = useState(null);
+
+  // const [mainThreadRes, setMainThreadRes] = useState(null);
+
+  // const fetchFromMainThread = async () => {
+  //   const res = await (self as any).doSomethingOnMainThread(clicks.div1)
+  //   setMainThreadRes(res)
+  // }
 
   const eventHandlerFactory = (key: keyof typeof clicks) => () => {
     setClicks((clicks) => ({
@@ -13,26 +25,23 @@ function ExampleApp({hostProps}: any) {
       [key]: clicks[key] + 1
     }));
   }
+
   return (
-    <div>
-      <h2>interact</h2>
+    <div style={{fontSize: 'xx-large'}}>
+      <style>
+        {css}
+      </style>
       <div onClick={eventHandlerFactory('div1')} style={{background: 'lime', padding: '5px', width: '50%'}}>
-        <button data-eject={true} onClick={eventHandlerFactory('button')}>click</button>
+        <button className={buttonClass} data-eject={true} onClick={eventHandlerFactory('button')}>click</button>
       </div>
-      <h2>state report</h2>
-      <div>
-        button was clicked {clicks.button} times
-      </div>
-      <div>
-        <span style={{background: 'lime'}}>lime</span> div was clicked {clicks.div1} times
-      </div>
-      <div>
-        <button onClick={async () => {
-          const res = await (self as any).doSomethingOnMainThread(clicks.div1)
-          setMainThreadRes(res)
-        }}>Ask Main thread to calc</button>
+      <ul>
+        <li>- <strong>button</strong> was clicked {clicks.button} times</li>
+        <li>- <strong>div</strong> and/or <strong>button</strong> were clicked {clicks.div1} times (button click event bubbled up)</li>
+      </ul>
+      {/* <div>
+        <button className={buttonClass} onClick={fetchFromMainThread}>Ask Main thread to calc</button>
         <div>{`main thread says: ${mainThreadRes}`}</div>
-      </div>
+      </div> */}
     </div>
   );
 }
